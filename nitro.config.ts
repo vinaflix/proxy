7a7554a785a2d76d071f1d2ce91be220287f2574
vinaman <quandabase+vinaman@gmail.com>
2024-05-25 15:19:23 +0700

    work in progress

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,6 +1,7 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: "server",
+  preset: "cloudflare-pages",
   routeRules: {
     '/example': { proxy: 'https://example.com' },
     '/yts/**': { proxy: 'https://yts.mx/api/v2/**' },
